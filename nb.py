6c81e4b69d0cdf171aeaa37f15cae5d42fa69c44
Lidan Yang <nellyyanglidan@yahoo.com>
2018-11-28 14:01:01 -0800

    multiple results for mlp. Add naive bayes

---
+++ nb.py
@@ -0,0 +1,76 @@
+from preprocess import PreProcess
+from eval import Evaluation
+from feature import Feature
+import numpy as np
+
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.preprocessing import StandardScaler
+
+class NB:
+    def __init__(self, pre):
+        self.pre = pre
+        self.dataset = pre.dataset
+        self.corpus = pre.corpus
+        self.labels = pre.labels
+
+        # constant variables for the whole collection
+        self.DICT_LABEL2INT = pre.DICT_LABEL2INT
+        self.subjects = pre.subjects
+
+    def dataset_gen(self, subject, valid=False):
+        self.X_train, self.X_test, self.X_dev, \
+        self.y_train, self.y_test, self.y_dev = self.pre.dataset_gen(subject, valid)
+
+    """
+    TODO: Pipeline is a classical class which could extract, select features and train the model with some classifier
+    Instruction for train_SVM:
+        (1) vectorize the corpus with word counts
+        (2) employ tf-idf transformation
+        (3) fit and predict with SVM model (which is optimized by SGDOptimizer)
+            Remeber to encode the label with +1/-1
+    """
+    def train(self, lda=False):
+        feature = Feature(trained=True)
+        classifier = MultinomialNB()
+
+        if lda:
+            model = Pipeline([('vectorized', feature.vector),
+                              ('tf-idf', feature.tfidftransform),
+                              ('lda', feature.ldatransform),
+                              ('scalar', StandardScaler(with_mean = False)),
+                              ('clf', classifier)])
+        else:
+            model = Pipeline([('vectorized', feature.vector),
+                              ('tf-idf', feature.tfidftransform),
+                              ('scalar', StandardScaler(with_mean = False)),
+                              ('clf', classifier)])
+
+        true, predicted = [], []
+        for i in range(len(self.subjects)):
+            # preprocess training and testing set
+            self.dataset_gen(subject=self.subjects[i], valid=False)
+
+            # train and predict
+            model.fit(self.X_train, self.y_train)
+
+            # store true labels and predictions
+            true.append(self.y_test)
+            predicted.append(model.predict(self.X_test))
+
+        # convert them to sparse matrix (N * L)
+        # matrix[i][j] = 1 indicates entry i has label j,
+        true_matrix, pred_matrix = np.array(true, int).T, np.array(predicted, int).T
+        true_matrix[true_matrix == -1] = 0
+        pred_matrix[pred_matrix == -1] = 0
+
+        evaluation = Evaluation(self.subjects)
+        evaluation.model_evaluate(true_matrix=true_matrix, pred_matrix=pred_matrix)
+
+
+if __name__ == '__main__':
+    preprocessor = PreProcess(root='./corpus/corpus.json', save='./corpus/corpus_nostopwords.json')
+    model = NB(preprocessor)
+    model.train(lda=False)
